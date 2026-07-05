8678a828f71e1d246161778fea6269c1a7e02aee
tdmcoder1337 <muhammadsodiq4322@gmail.com>
2026-07-05 15:59:30 +0500

    vercel server qoyildi

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(
   cors({
     origin: [
-      process.env.CLIENT_URL || 'http://localhost:5173',
+      process.env.CLIENT_URL,
       'http://localhost:5173',
       'http://localhost:5174',
     ],
